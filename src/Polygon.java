6e3d372abf7b9e627de6295217daa1e8deaee5a9
amressammm <47894482+amressammm@users.noreply.github.com>
2020-04-05 18:06:33 +0200

    Update Polygon.java

--- src/Polygon.java
+++ src/Polygon.java
@@ -1,8 +1,14 @@
-
-public class Polygon {
+public class Polygon extends java.awt.Polygon implements Comparable  {
 
 	public Polygon() {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
+
